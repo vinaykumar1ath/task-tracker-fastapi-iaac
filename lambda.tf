50a1ab3b75eca7642d77f7c6b9ff5cf0277b70d2
vinaykumar1ath <vinaykumar1ath@gmail.com>
2026-06-21 18:01:36 +0530

    added lambda function_name output

--- lambda.tf
+++ lambda.tf
@@ -50,3 +50,7 @@ output "lambda_arn" {
 output "lambda_invoke_arn" {
   value = aws_lambda_function.task_tracker_fastapi.invoke_arn
 }
+
+output "lambda_function_name" {
+  value = aws_lambda_function.task_tracker_fastapi.function_name
+}
